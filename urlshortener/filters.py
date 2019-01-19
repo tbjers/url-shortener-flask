b2a8d5896fcf285d0e63f40334ecf408cecada5d
Torgny Bjers <torgny@xorcode.com>
2019-01-19 01:54:25 -0500

    support number format and relative date
    - add filter for number format, support number, decimal and percent
    - add filter for relative time format
    - update templates to use number format and relative dates

--- urlshortener/filters.py
+++ urlshortener/filters.py
@@ -1,4 +1,6 @@
-from babel.dates import format_datetime
+from datetime import timedelta, datetime
+from babel.numbers import format_number, format_decimal, format_percent
+from babel.dates import format_datetime, format_timedelta
 
 def _format_datetime(value, format='medium'):
     if format == 'full':
@@ -7,5 +9,18 @@ def _format_datetime(value, format='medium'):
         format="dd.MM.y HH:mm"
     return format_datetime(value, format)
 
+def _format_number(value, format='number'):
+    if format == 'decimal':
+        return format_decimal(value)
+    elif format == 'percent':
+        return format_percent(value)
+    return format_number(value)
+
+def _format_relative_date(value):
+    delta = value - datetime.today()
+    return format_timedelta(timedelta(days=delta.days, seconds=delta.seconds), add_direction=True)
+
 def init_filters(app):
     app.jinja_env.filters['datetime'] = _format_datetime
+    app.jinja_env.filters['number'] = _format_number
+    app.jinja_env.filters['relative'] = _format_relative_date
